637f7d0735a1c3c4213e7ff77387f11ef504dc37
sambreece <Samantha.Breece@tlgcohort.com>
2023-02-01 19:16:13 -0800

    stored user input and cleared form

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,22 @@
 destination_form.addEventListener("submit",(event) => {
     event.preventDefault();
     document.body.style.backgroundColor = 'gray';
+    console.log(getUserInput());
 });
+
+function getUserInput(){
+    //Store user input
+    const userInput = {
+    name: document.querySelector("#destination_name").value,
+    location: document.querySelector("#destination_location").value,
+    photo: document.querySelector("#destination_photo").value,
+    description: document.querySelector("#destination_description").value
+    };
+
+    //Clear form
+    document.querySelector("#destination_name").value = "";
+    document.querySelector("#destination_location").value ="";
+    document.querySelector("#destination_photo").value = "";
+    document.querySelector("#destination_description").value = "";
+    return userInput;
+}
